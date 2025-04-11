9260e6f8cb72e23dfef20636f97fe52e9bb682a8
kamangir <arash@kamangir.net>
2025-04-11 16:36:49 -0700

    rebuild - kamangir/bolt#746

--- bluer_ai/.abcli/tests/unpack.sh
+++ bluer_ai/.abcli/tests/unpack.sh
@@ -2,6 +2,16 @@
 
 function test_bluer_ai_unpack_repo_name() {
     bluer_ai_assert \
+        $(bluer_ai_unpack_repo_name bluer-ai) \
+        bluer-ai
+    [[ $? -ne 0 ]] && return 1
+
+    bluer_ai_assert \
         $(bluer_ai_unpack_repo_name bluer_ai) \
+        bluer-ai
+    [[ $? -ne 0 ]] && return 1
+
+    bluer_ai_assert \
+        $(bluer_ai_unpack_repo_name @ai) \
         bluer-ai
 }
