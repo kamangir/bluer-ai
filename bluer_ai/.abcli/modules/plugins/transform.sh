c4a2f983e22eb85fe4d29612ff4e45aaf54ffd92
kamangir <arash@kamangir.net>
2025-04-24 19:40:22 -0700

    start of plugins_transform-2025-04-24-5k6vjz 🌀 - kamangir/bolt#746

--- bluer_ai/.abcli/modules/plugins/transform.sh
+++ bluer_ai/.abcli/modules/plugins/transform.sh
@@ -8,15 +8,22 @@ function bluer_ai_plugins_transform() {
     fi
     local plugin_name=$(bluer_ai_plugin_name_from_repo $repo_name)
 
+    if [[ ! -d "$abcli_path_git/$repo_name" ]]; then
+        bluer_ai_log_error "@plugins: transform: $repo_name: repo not found."
+        return 1
+    fi
+
     bluer_ai_log "bluer-plugin -> $repo_name ($plugin_name)"
 
     pushd $abcli_path_git/$repo_name >/dev/null
 
     git mv bluer_plugin $plugin_name
+    [[ $? -ne 0 ]] && return 1
 
     git mv \
         $plugin_name/.abcli/bluer_plugin.sh \
         $plugin_name/.abcli/$plugin_name.sh
+    [[ $? -ne 0 ]] && return 1
 
     python3 -m bluer_ai.plugins \
         transform \
