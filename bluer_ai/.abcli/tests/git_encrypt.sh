1e464db6d9bf9c4b8f78a9f0f001f42546626730
kamangir <arash@kamangir.net>
2025-08-22 15:46:25 +0330

    ++ - kamangir/bolt#746

--- bluer_ai/.abcli/tests/git_encrypt.sh
+++ bluer_ai/.abcli/tests/git_encrypt.sh
@@ -3,7 +3,13 @@
 function test_bluer_ai_git_encrypt() {
     local options=$1
 
-    bluer_ai_git_clone bluer-plugin
+    local object_name=test_bluer_ai_git_encrypt-$(bluer_ai_string_timestamp_short)
+
+    bluer_ai_select $object_name
+
+    bluer_ai_git_clone \
+        bluer-plugin \
+        cd,object
     [[ $? -ne 0 ]] && return 1
 
     bluer_ai_git_encrypt
