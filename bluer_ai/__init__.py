715aea9a42ea9f30bf9d1ac3d2dfd9460209de04
kamangir <arash@kamangir.net>
2025-11-19 16:54:54 +0330

    ++ - kamangir/bolt#746

--- bluer_ai/__init__.py
+++ bluer_ai/__init__.py
@@ -6,7 +6,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} A language to speak AI."
 
-VERSION = "12.325.1"
+VERSION = "12.326.1"
 
 REPO_NAME = "bluer-ai"
 
--- bluer_ai/modules/terraform/functions.py
+++ bluer_ai/modules/terraform/functions.py
@@ -35,21 +35,29 @@ def poster(filename: str) -> bool:
 
     logger.info("{}.poster({})".format(NAME, filename))
 
-    image = add_frame(
-        np.concatenate(
-            [
-                render_text(
-                    centered=True,
-                    image_width=screen.get_size()[1],
-                    text=line,
-                    thickness=4,
-                )
-                for line in signature()
-            ],
-            axis=0,
-        ),
-        32,
+    image = np.concatenate(
+        [
+            render_text(
+                text=line,
+                centered=True,
+                image_width=screen.get_size()[1],
+                thickness=4,
+            )
+            for line in signature()
+        ],
+        axis=0,
     )
+
+    image = render_text(
+        image=image,
+        text="booting @ai ...",
+        centered=True,
+        font_color=3 * (200,),
+        thickness=12,
+        font_size=4,
+    )
+
+    image = add_frame(image, 32)
 
     return image if filename is None else file.save_image(filename, image)
 
