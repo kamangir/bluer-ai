16e131bb684db48fc28706a2b0cababd818de54e
kamangir <arash@kamangir.net>
2026-01-09 14:35:49 +0330

    ++

--- bluer_ai/.abcli/plugins/wifi/is_online.sh
+++ bluer_ai/.abcli/plugins/wifi/is_online.sh
@@ -3,23 +3,24 @@
 function bluer_ai_wifi_is_online() {
     local output=1
 
-    local filename=~/$abcli_path_temp/is_online-$(bluer_ai_string_timestamp)
+    local filename=$abcli_path_temp/is_online-$(bluer_ai_string_timestamp)
 
-    curl -L https://github.com/kamangir \
+    curl -L https://arvancloud.ir \
         --output $filename \
         --connect-timeout 3
-    [[ $? -ne 0 ]] &&
-        output=0
 
-    [[ -f "$filename" ]] &&
-        rm -v $filename
+    if [[ -f "$filename" ]]; then
+        rm $filename
+    else
+        output=0
+    fi
 
     echo $output
 }
 
 export BLUER_AI_IS_ONLINE=$(bluer_ai_wifi_is_online)
 
-if [[ "$BLUER_AI_IS_ONLINE" == true ]]; then
+if [[ "$BLUER_AI_IS_ONLINE" == 1 ]]; then
     bluer_ai_log "🛜 online."
 else
     bluer_ai_log "⛓️‍💥 offline."
